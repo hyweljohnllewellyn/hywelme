2ed4515b8266b10d7867c13c2d31d97138520a07
Hywel Llewellyn <hywel.llewellyn@gmail.com>
2016-10-03 20:06:01 +0100

    updated region

--- javascript/apiGateway-js-sdk/apigClient.js
+++ javascript/apiGateway-js-sdk/apigClient.js
@@ -21,7 +21,7 @@ apigClientFactory.newClient = function (config) {
             accessKey: '',
             secretKey: '',
             sessionToken: '',
-            region: '',
+            region: 'eu-west-1',
             apiKey: undefined,
             defaultContentType: 'application/json',
             defaultAcceptType: 'application/json'
@@ -51,7 +51,7 @@ apigClientFactory.newClient = function (config) {
         config.defaultAcceptType = 'application/json';
     }
 
-    
+
     // extract endpoint and path from url
     var invokeUrl = 'https://nw1ryiksz3.execute-api.eu-west-1.amazonaws.com/hywelme';
     var endpoint = /(^https?:\/\/[^\/]+)/g.exec(invokeUrl)[1];
@@ -80,14 +80,14 @@ apigClientFactory.newClient = function (config) {
     };
 
     var apiGatewayClient = apiGateway.core.apiGatewayClientFactory.newClient(simpleHttpClientConfig, sigV4ClientConfig);
-    
-    
-    
+
+
+
     apigClient.rootOptions = function (params, body, additionalParams) {
         if(additionalParams === undefined) { additionalParams = {}; }
-        
+
         apiGateway.core.utils.assertParametersDefined(params, [], ['body']);
-        
+
         var rootOptionsRequest = {
             verb: 'options'.toUpperCase(),
             path: pathComponent + uritemplate('/').expand(apiGateway.core.utils.parseParametersToObject(params, [])),
@@ -95,17 +95,17 @@ apigClientFactory.newClient = function (config) {
             queryParams: apiGateway.core.utils.parseParametersToObject(params, []),
             body: body
         };
-        
-        
+
+
         return apiGatewayClient.makeRequest(rootOptionsRequest, authType, additionalParams, config.apiKey);
     };
-    
-    
+
+
     apigClient.autoquotePost = function (params, body, additionalParams) {
         if(additionalParams === undefined) { additionalParams = {}; }
-        
+
         apiGateway.core.utils.assertParametersDefined(params, [], ['body']);
-        
+
         var autoquotePostRequest = {
             verb: 'post'.toUpperCase(),
             path: pathComponent + uritemplate('/autoquote').expand(apiGateway.core.utils.parseParametersToObject(params, [])),
@@ -113,11 +113,11 @@ apigClientFactory.newClient = function (config) {
             queryParams: apiGateway.core.utils.parseParametersToObject(params, []),
             body: body
         };
-        
-        
+
+
         return apiGatewayClient.makeRequest(autoquotePostRequest, authType, additionalParams, config.apiKey);
     };
-    
+
 
     return apigClient;
 };
